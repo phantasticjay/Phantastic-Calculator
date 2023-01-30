a1ce3696f42177779ac73411574baf99ac038627
Jay Phan <jay.phan@outlook.com>
2023-01-30 20:09:02 +1100

    Changing calculator appearance
    Changing the theme from darkblack 1 to black
    Overall button colours to a dark gray, the expressions to a light red and the 3 most upper buttons to a lighter gray. This will provide the calculator with more colour for accessibility purposes

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkBlack')  # Using a dark mode theme for this project
+sg.theme('Black')  # Using a dark mode theme for this project
 
-layout = [[sg.Input(justification='right', font=[30], key='input', size=(5, 5), expand_x=True)],
-          [sg.Button('C'), sg.Button('%'), sg.Button('DEL'), sg.Button('÷')],
-          [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('X')],
-          [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-')],
-          [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+')],
-          [sg.Button('0', expand_x=True), sg.Button('.'), sg.Button('=')]]
+layout = [[sg.Input(justification='right', font=[30], key='input', size=(5, 5), expand_x=True, background_color='black')],
+          [sg.Button('C', button_color='#777777'), sg.Button('%', button_color='#777777'), sg.Button('DEL', button_color='#777777'), sg.Button('÷', button_color='#FF6666')],
+          [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('x', button_color='#FF6666')],
+          [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-', button_color='#FF6666')],
+          [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+', button_color='#FF6666')],
+          [sg.Button('0', expand_x=True), sg.Button('.'), sg.Button('=', button_color='#FF6666')]]
 
 window = sg.Window('Phantastic Calculator', layout, auto_size_buttons=False,
-                   default_button_element_size=(7, 3), element_justification='right')
+                   default_button_element_size=(7, 3), element_justification='right',
+                   button_color=('white', '#333333'))
 
 keys_entered = ''
 while True:
@@ -21,7 +22,7 @@ while True:
         keys_entered = ''
     if event == 'DEL':  # Deletes the most recent input
         keys_entered = keys_entered[:-1]
-    if event in '1234567890+-X%.÷':  # If any of the buttons are pushed and contain any of these elements, they show up on the text box
+    if event in '1234567890+-x%.÷':  # If any of the buttons are pushed and contain any of these elements, they show up on the text box
         keys_entered = values['input']
         keys_entered += event
 
@@ -41,48 +42,45 @@ while True:
             keyList = keys_entered.split('-')  # Splits the calculator input into a list by the - sign so only numbers remain
             sub = float(keyList[0])
             for i in range(1, len(keyList)):
-                sub = sub - float(keyList[i])  # Using a for loop to subtract the numbers in the list
+                sub = sub - float(keyList[i])
 
             if sub.is_integer():
                 keys_entered = int(sub)
             else:
-                keys_entered = sub  # Displays the result onto the calculator screen
+                keys_entered = sub
 
-        elif 'X' in keys_entered:  # Finds if the calculator contains any input of the X sign
-            keyList = keys_entered.split('X')  # Splits the calculator input into a list by the X sign so only numbers remain
-            print(keyList)
+        elif 'x' in keys_entered:  # Finds if the calculator contains any input of the X sign
+            keyList = keys_entered.split('x')  # Splits the calculator input into a list by the X sign so only numbers remain
             multi = float(keyList[0])
             for i in range(1, len(keyList)):
-                multi = multi * float(keyList[i])  # Using a for loop to multiply the numbers in the list
+                multi = multi * float(keyList[i])
 
             if multi.is_integer():
                 keys_entered = int(multi)
             else:
-                keys_entered = multi  # Displays the result onto the calculator screen
+                keys_entered = multi
 
         elif '%' in keys_entered:  # Finds if the calculator contains any input of the % sign
             keyList = keys_entered.split('%')  # Splits the calculator input into a list by the % sign so only numbers remain
-            print(keyList)
             mod = float(keyList[0])
             for i in range(1, len(keyList)):
-                mod = mod % float(keyList[i])  # Using a for loop to modulo the numbers in the loop
+                mod = mod % float(keyList[i])
 
             if mod.is_integer():
                 keys_entered = int(mod)
             else:
-                keys_entered = mod  # Displays the result onto the calculator screen
+                keys_entered = mod
 
         elif '÷' in keys_entered:  # Finds if the calculator contains any input of the ÷ sign
             keyList = keys_entered.split('÷')  # Splits the calculator input into a list by the ÷ sign so only numbers remain
-            print(keyList)
             div = float(keyList[0])
             for i in range(1, len(keyList)):
-                div = div / float(keyList[i])  # Using a for loop to divide the numbers in the list
+                div = div / float(keyList[i])
 
             if div.is_integer():
                 keys_entered = int(div)
             else:
-                keys_entered = div  # Displays the result onto the calculator screen
+                keys_entered = div
 
     window['input'].update(keys_entered)  # Updates the calculator screen
 
