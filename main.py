4fbd59f2b8281b8113920c4da95588d2f902ca79
Jay Phan <jay.phan@outlook.com>
2023-01-30 14:32:46 +1100

    Allowing float/decimal values
    Changing the initial numbers to a float and checking if integer at the end of the loop. This allows decimals to work within the calculator

--- main.py
+++ main.py
@@ -27,52 +27,62 @@ while True:
 
     if event == '=':
         if '+' in keys_entered:  # Finds if the calculator contains any input of the + sign
-            keyList = keys_entered.split(
-                '+')  # Splits the calculator input into a list by the + sign so only numbers remain
+            keyList = keys_entered.split('+')  # Splits the calculator input into a list by the + sign so only numbers remain
             add = 0
             for i in range(len(keyList)):
-                add = add + int(keyList[i])  # Using a for loop to sum the numbers in the list
+                add = add + float(keyList[i])  # Using a for loop to sum the numbers in the list
 
-            keys_entered = add  # Displays the result onto the calculator screen
+            if add.is_integer():  # Checking if the final result is an integer
+                keys_entered = int(add)  # if integer, then it will adjust the final result to not contain a '.0' after
+            else:
+                keys_entered = add  # Displays the result onto the calculator screen
 
         elif '-' in keys_entered:  # Finds if the calculator contains any input of the - sign
-            keyList = keys_entered.split(
-                '-')  # Splits the calculator input into a list by the - sign so only numbers remain
-            sub = int(keyList[0])
+            keyList = keys_entered.split('-')  # Splits the calculator input into a list by the - sign so only numbers remain
+            sub = float(keyList[0])
             for i in range(1, len(keyList)):
-                sub = sub - int(keyList[i])  # Using a for loop to subtract the numbers in the list
+                sub = sub - float(keyList[i])  # Using a for loop to subtract the numbers in the list
 
-            keys_entered = sub  # Displays the result onto the calculator screen
+            if sub.is_integer():
+                keys_entered = int(sub)
+            else:
+                keys_entered = sub  # Displays the result onto the calculator screen
 
         elif 'X' in keys_entered:  # Finds if the calculator contains any input of the X sign
-            keyList = keys_entered.split(
-                'X')  # Splits the calculator input into a list by the X sign so only numbers remain
+            keyList = keys_entered.split('X')  # Splits the calculator input into a list by the X sign so only numbers remain
             print(keyList)
-            multi = int(keyList[0])
+            multi = float(keyList[0])
             for i in range(1, len(keyList)):
-                multi = multi * int(keyList[i])  # Using a for loop to multiply the numbers in the list
+                multi = multi * float(keyList[i])  # Using a for loop to multiply the numbers in the list
 
-            keys_entered = multi  # Displays the result onto the calculator screen
+            if multi.is_integer():
+                keys_entered = int(multi)
+            else:
+                keys_entered = multi  # Displays the result onto the calculator screen
 
         elif '%' in keys_entered:  # Finds if the calculator contains any input of the % sign
-            keyList = keys_entered.split(
-                '%')  # Splits the calculator input into a list by the % sign so only numbers remain
+            keyList = keys_entered.split('%')  # Splits the calculator input into a list by the % sign so only numbers remain
             print(keyList)
-            mod = int(keyList[0])
+            mod = float(keyList[0])
             for i in range(1, len(keyList)):
-                mod = mod % int(keyList[i])  # Using a for loop to modulo the numbers in the loop
+                mod = mod % float(keyList[i])  # Using a for loop to modulo the numbers in the loop
 
-            keys_entered = mod  # Displays the result onto the calculator screen
+            if mod.is_integer():
+                keys_entered = int(mod)
+            else:
+                keys_entered = mod  # Displays the result onto the calculator screen
 
         elif '÷' in keys_entered:  # Finds if the calculator contains any input of the ÷ sign
-            keyList = keys_entered.split(
-                '÷')  # Splits the calculator input into a list by the ÷ sign so only numbers remain
+            keyList = keys_entered.split('÷')  # Splits the calculator input into a list by the ÷ sign so only numbers remain
             print(keyList)
-            div = int(keyList[0])
+            div = float(keyList[0])
             for i in range(1, len(keyList)):
-                div = div / int(keyList[i])  # Using a for loop to divide the numbers in the list
+                div = div / float(keyList[i])  # Using a for loop to divide the numbers in the list
 
-            keys_entered = div  # Displays the result onto the calculator screen
+            if div.is_integer():
+                keys_entered = int(div)
+            else:
+                keys_entered = div  # Displays the result onto the calculator screen
 
     window['input'].update(keys_entered)  # Updates the calculator screen
 
