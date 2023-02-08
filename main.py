d96d3cd4b336e3e2c9bdca60cd3e0bc32fc7b6e1
Jay Phan <jay.phan@outlook.com>
2023-02-08 20:08:48 +1100

    Added more functions
    - Square root
    - Square inputs
    - 1 divide input
    Influenced by windows calculator

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import PySimpleGUI as sg
+import math
 
 sg.theme('Black')  # Using a dark mode theme for this project
 
 layout = [[sg.Input(justification='right', font=[30], key='input', size=(5, 5), expand_x=True, background_color='black')],
-          [sg.Button('C', button_color='#777777'), sg.Button('%', button_color='#777777'), sg.Button('DEL', button_color='#777777'), sg.Button('÷', button_color='#FF6666')],
+          [sg.Button('C', button_color='#777777'), sg.Button('%', button_color='#777777'), sg.Button('DEL', button_color='#777777')],
+          [sg.Button('¹/x'), sg.Button('x²'), sg.Button('√'), sg.Button('÷', button_color='#FF6666')],
           [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('x', button_color='#FF6666')],
           [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-', button_color='#FF6666')],
           [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+', button_color='#FF6666')],
@@ -30,6 +32,15 @@ while True:
         keys_enteredFormat = keys_entered.replace('x', '*')  # Replaces the x in the string to a * to make the eval() function work
         keys_enteredFormatv2 = keys_enteredFormat.replace('÷', '/')  # Replaces the ÷ in the string to a / to make the eval() function work
         keys_entered = eval(str(keys_enteredFormatv2))  # Calculates the entire string
+
+    if event == '√':
+        keys_entered = math.sqrt(int(keys_entered))
+
+    if event == 'x²':
+        keys_entered = int(keys_entered) * int(keys_entered)
+
+    if event == '¹/x':
+        keys_entered = 1/int(keys_entered)
 
     window['input'].update(keys_entered)  # Updates the calculator screen
 
