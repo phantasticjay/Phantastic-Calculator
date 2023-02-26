e7c5ab22c66a21562cf9b08f36b8332a8c4c4ae1
Jay Phan <jay.phan@outlook.com>
2023-02-26 15:08:14 +1100

    Button colour fixes

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import statistics
 sg.theme('Black')  # Using a dark mode theme for this project
 
 layout = [[sg.Input(justification='right', font=[30], key='input', size=(5, 5), expand_x=True, background_color='black')],
-          [sg.Button('mean'), sg.Button('median'), sg.Button('mode'), sg.Button('C', button_color='#777777'), sg.Button('DEL', button_color='#777777')],
-          [sg.Button('sin'), sg.Button('¹/x'), sg.Button('x²'), sg.Button('√'), sg.Button('÷', button_color='#FF6666')],
-          [sg.Button('cos'), sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('x', button_color='#FF6666')],
-          [sg.Button('tan'), sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-', button_color='#FF6666')],
-          [sg.Button('log'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+', button_color='#FF6666')],
-          [sg.Button('ln'), sg.Button('0'), sg.Button('%'), sg.Button('.'), sg.Button('=', button_color='#FF6666')]]
+          [sg.Button('mean', button_color='#777777'), sg.Button('median', button_color='#777777'), sg.Button('mode', button_color='#777777'), sg.Button('C', button_color='#777777'), sg.Button('DEL', button_color='#777777')],
+          [sg.Button('sin', button_color='#777777'), sg.Button('¹/x', button_color='#777777'), sg.Button('x²', button_color='#777777'), sg.Button('√', button_color='#777777'), sg.Button('÷', button_color='#FF6666')],
+          [sg.Button('cos', button_color='#777777'), sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('x', button_color='#FF6666')],
+          [sg.Button('tan', button_color='#777777'), sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-', button_color='#FF6666')],
+          [sg.Button('log', button_color='#777777'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+', button_color='#FF6666')],
+          [sg.Button('ln', button_color='#777777'), sg.Button('0'), sg.Button('%'), sg.Button('.'), sg.Button('=', button_color='#FF6666')]]
 
 window = sg.Window('Phantastic Calculator', layout, auto_size_buttons=False,
                    default_button_element_size=(7, 3), element_justification='right',
