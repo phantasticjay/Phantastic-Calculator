87dc448efb5af1dc139ec9dbbc88958864daef3e
Jay Phan <jay.phan@outlook.com>
2023-02-26 15:06:01 +1100

    Adding statistical functions
    Mean, Median and Mode functions are integrated into the calculator

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import PySimpleGUI as sg
 import math
+import re
+import statistics
 
 sg.theme('Black')  # Using a dark mode theme for this project
 
 layout = [[sg.Input(justification='right', font=[30], key='input', size=(5, 5), expand_x=True, background_color='black')],
-          [sg.Button('C', button_color='#777777'), sg.Button('%', button_color='#777777'), sg.Button('DEL', button_color='#777777')],
+          [sg.Button('mean'), sg.Button('median'), sg.Button('mode'), sg.Button('C', button_color='#777777'), sg.Button('DEL', button_color='#777777')],
           [sg.Button('sin'), sg.Button('¹/x'), sg.Button('x²'), sg.Button('√'), sg.Button('÷', button_color='#FF6666')],
           [sg.Button('cos'), sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('x', button_color='#FF6666')],
           [sg.Button('tan'), sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-', button_color='#FF6666')],
           [sg.Button('log'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+', button_color='#FF6666')],
-          [sg.Button('ln'), sg.Button('0', expand_x=True), sg.Button('.'), sg.Button('=', button_color='#FF6666')]]
+          [sg.Button('ln'), sg.Button('0'), sg.Button('%'), sg.Button('.'), sg.Button('=', button_color='#FF6666')]]
 
 window = sg.Window('Phantastic Calculator', layout, auto_size_buttons=False,
                    default_button_element_size=(7, 3), element_justification='right',
@@ -56,6 +58,22 @@ while True:
 
     if event == 'tan':
         keys_entered = math.tan(math.radians(int(keys_entered)))
+
+    if event == 'mean':
+        keyMean = keys_entered.split('+')
+        keyMean = [int(i) for i in keyMean]
+        keys_entered = statistics.mean(keyMean)
+
+    if event == 'median':
+        keyMedian = keys_entered.split('+')
+        keyMedian = [int(i) for i in keyMedian]
+        keys_entered = statistics.median(keyMedian)
+
+    if event == 'mode':
+        keyMode = keys_entered.split('+')
+        keyMode = [int(i) for i in keyMode]
+        keys_entered = statistics.mode(keyMode)
+
 
     window['input'].update(keys_entered)  # Updates the calculator screen
 
